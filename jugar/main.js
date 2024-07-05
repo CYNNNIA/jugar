6fa9a64e778f48ba0fea1125e0b22bfb6e658db6
cynnnia <cynthialorenzolopez@gmail.com>
2024-07-05 10:02:38 +0200

    .

--- jugar/main.js
+++ jugar/main.js
@@ -1,5 +1,4 @@
 import { Header } from '../src/components/Header/Header'
-import { initMole } from '../src/pages/WackAMole/WackAMole'
 import './style.css'
 
 const divApp = document.querySelector('#app')
@@ -10,6 +9,3 @@ const divContent = document.createElement('div')
 divContent.className = 'content'
 
 divApp.append(divContent)
-
-//lo siguiente es temporal y hay que borrarlo es para que salga todo el rato en pantalla el arcoiris al recargar la pagina para que sea mas facil trabajar
-initMole()
--- src/pages/Snake/Snake.js
+++ src/pages/Snake/Snake.js
@@ -1,8 +1,98 @@
+// src/pages/Snake/Snake.js
 import './Snake.css'
 
 export const initSnake = () => {
   const divContent = document.querySelector('.content')
   divContent.innerHTML = ''
 
-  divContent.innerHTML = `<h1>Snake</h1>`
+  const snake = [{ x: 200, y: 200 }]
+  let direction = { x: 0, y: 0 }
+  let food = { x: getRandomCoordinate(), y: getRandomCoordinate() }
+  let speed = 100
+  let gameInterval
+
+  const createDiv = (className, x, y) => {
+    const div = document.createElement('div')
+    div.className = className
+    div.style.left = `${x}px`
+    div.style.top = `${y}px`
+    return div
+  }
+
+  const drawSnake = () => {
+    snake.forEach((segment) => {
+      const snakeElement = createDiv('snake', segment.x, segment.y)
+      divContent.appendChild(snakeElement)
+    })
+  }
+
+  const drawFood = () => {
+    const foodElement = createDiv('food', food.x, food.y)
+    divContent.appendChild(foodElement)
+  }
+
+  const getRandomCoordinate = () => {
+    return Math.floor(Math.random() * 20) * 20
+  }
+
+  const moveSnake = () => {
+    const head = { x: snake[0].x + direction.x, y: snake[0].y + direction.y }
+    snake.unshift(head)
+
+    if (head.x === food.x && head.y === food.y) {
+      food = { x: getRandomCoordinate(), y: getRandomCoordinate() }
+    } else {
+      snake.pop()
+    }
+  }
+
+  const checkCollision = () => {
+    const head = snake[0]
+
+    if (head.x < 0 || head.x >= 400 || head.y < 0 || head.y >= 400) {
+      clearInterval(gameInterval)
+      alert('Game Over')
+      return true
+    }
+
+    for (let i = 1; i < snake.length; i++) {
+      if (snake[i].x === head.x && snake[i].y === head.y) {
+        clearInterval(gameInterval)
+        alert('Game Over')
+        return true
+      }
+    }
+
+    return false
+  }
+
+  const gameLoop = () => {
+    moveSnake()
+
+    if (checkCollision()) return
+
+    divContent.innerHTML = ''
+    drawSnake()
+    drawFood()
+  }
+
+  const handleKeyPress = (event) => {
+    switch (event.key) {
+      case 'ArrowUp':
+        if (direction.y === 0) direction = { x: 0, y: -20 }
+        break
+      case 'ArrowDown':
+        if (direction.y === 0) direction = { x: 0, y: 20 }
+        break
+      case 'ArrowLeft':
+        if (direction.x === 0) direction = { x: -20, y: 0 }
+        break
+      case 'ArrowRight':
+        if (direction.x === 0) direction = { x: 20, y: 0 }
+        break
+    }
+  }
+
+  document.addEventListener('keydown', handleKeyPress)
+  gameInterval = setInterval(gameLoop, speed)
 }
